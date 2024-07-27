479cd42881a5d38b81d5f23163e99c873816e5c5
LeoLi04 <leoli_4@163.com>
2024-07-27 16:23:53 +0800

    feat:mybatis-plus配置

--- src/main/java/com/leoli04/springboottemplate/SpringBootTemplateApplication.java
+++ src/main/java/com/leoli04/springboottemplate/SpringBootTemplateApplication.java
@@ -1,8 +1,10 @@
 package com.leoli04.springboottemplate;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(basePackages = "com.leoli04.springboottemplate.repository.dao")
 @SpringBootApplication
 public class SpringBootTemplateApplication {
 
---
+++ src/main/java/com/leoli04/springboottemplate/common/config/MybatisPlusConfig.java
@@ -0,0 +1,29 @@
+package com.leoli04.springboottemplate.common.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description: mybatis-plus配置
+ * 参考地址：https://baomidou.com/introduce/
+ * @Author: LeoLi04
+ * @CreateDate: 2024/7/27 16:17
+ * @Version: 1.0
+ */
+@Configuration
+public class MybatisPlusConfig {
+    /**
+     * 添加分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 如果配置多个插件, 切记分页最后添加
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 如果有多数据源可以不配具体类型, 否则都建议配上具体的 DbType
+        return interceptor;
+    }
+}
